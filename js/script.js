02399f376adb1c2f871b6b96842ed698a0c9213d
Dmytro Bula <dmytro.bula21@gmail.com>
2020-07-31 17:52:16 +0100

    added three functions: showPage function, appendPageLinks function and search function. All the functionality is added

--- js/script.js
+++ js/script.js
@@ -2,49 +2,99 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+// list variables
+const list = document.querySelector(".student-list");
+const listItems = [...list.children];
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+let page = 1;
+const rows = 10;
+const pages = Math.ceil(listItems.length / rows);
 
-
-
+// hide all the list items
+for (let i = 0; i < listItems.length; i++) {
+  listItems[i].style.display = "none";
+}
 
 /*** 
-   Create the `showPage` function to hide all of the items in the 
+   showPage function to hide all of the items in the 
    list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
 ***/
+const showPage = (listItems, page, rows) => {
+  const trimStart = (page - 1) * rows;
+  const trimEnd = trimStart + rows;
 
+  const trimmedData = listItems.slice(trimStart, trimEnd);
 
-
+  // display the links that are on the current page
+  for (let i = 0; i < trimmedData.length; i++) {
+    trimmedData[i].style.display = "block";
+  }
+};
+// call the function on page 1 before the page changes
+showPage(listItems, page, rows);
 
 /*** 
-   Create the `appendPageLinks function` to generate, append, and add 
+   appendPageLinks function to generate, append, and add 
    functionality to the pagination buttons.
 ***/
 
+function appendPageLinks(listItems) {
+  const pageDiv = document.querySelector(".page");
+  const div = document.createElement("div");
+  div.className = "pagination";
+  pageDiv.appendChild(div);
 
+  const ul = document.createElement("ul");
+  div.appendChild(ul);
 
+  // loop thorough the amount of pages to create buttons
+  for (let i = 1; i <= pages; i++) {
+    const li = document.createElement("li");
+    const btn = document.createElement("a");
 
+    btn.innerText = i;
+    li.appendChild(btn);
+    ul.appendChild(li);
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+    // assign an active class to the first button
+    btn.innerText == page ? (btn.className = "active") : (btn.className = "");
+
+    // adding an event listener to the button
+    btn.addEventListener("click", () => {
+      // hide the items that are already on the page
+      listItems.map((item) => {
+        item.style.display = "none";
+      });
+      // change the class to the button that is selected
+      let previousBtn = document.querySelector(".active");
+      previousBtn.className = "";
+      btn.className = "active";
+      page = i;
+      // switch to next page
+      showPage(listItems, page, rows);
+    });
+  }
+}
+appendPageLinks(listItems);
+
+// search function
+function search() {
+  const searchDiv = document.querySelector(".student-search");
+  const input = searchDiv.querySelector("input");
+  input.onkeyup = () => {
+    const filter = input.value.toUpperCase();
+
+    for (let i = 0; i < listItems.length; i++) {
+      const name = listItems[i].querySelector("h3");
+      const textValue = name.innerText || name.textContent;
+
+      if (textValue.toUpperCase().indexOf(filter) > -1) {
+        listItems[i].style.display = "";
+      } else {
+        listItems[i].style.display = "none";
+      }
+    }
+  };
+}
+search();
